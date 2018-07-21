b6f6a4b5a4121efc05daac9b7eba06e55d2830e3
Otabek Kamalov <otakam86@gmail.com>
2018-07-21 01:42:48 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,11 +10,11 @@ def array_with_two_elements
   array
 end
 
-def first_element(array)
+def first_element (array)
 array[0] 
 end
 
-def third_element(array)
+def third_elemen t(array)
   array[2]
 end 
 
@@ -22,7 +22,7 @@ def last_element (array)
   array.[-1] 
 end
 
-def first_element_with_array_methods(array)
+def first_element_with_array_methods (array)
   array.first
 end
 
@@ -30,6 +30,6 @@ def last_element_with_array_methods (array)
   array.last
 end
 
-def length_of_array(array)
+def length_of_array (array)
   array.size 
 end
